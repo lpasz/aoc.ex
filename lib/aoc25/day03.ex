e171d3c9bc66fab95e9af675ecf44f3d1305ed9a
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2025-12-04 09:39:44 -0300

    Updated to Day 03 to use main functions

--- lib/aoc25/day03.ex
+++ lib/aoc25/day03.ex
@@ -4,7 +4,7 @@ defmodule Aoc25.Day03 do
     file_path
     |> parse()
     |> Enum.map(&max_seq_number_of(&1, 2))
-    |> Enum.map(&Aoc.digits_to_number/1)
+    |> Enum.map(&Integer.undigits/1)
     |> Enum.sum()
   end
 
@@ -12,7 +12,7 @@ defmodule Aoc25.Day03 do
     file_path
     |> parse()
     |> Enum.map(&max_seq_number_of(&1, 12))
-    |> Enum.map(&Aoc.digits_to_number/1)
+    |> Enum.map(&Integer.undigits/1)
     |> Enum.sum()
   end
 
@@ -20,8 +20,8 @@ defmodule Aoc25.Day03 do
     file_path
     |> File.read!()
     |> String.split("\n", trim: true)
-    |> Enum.map(&Regex.scan(~r/\d/, &1))
-    |> Enum.map(fn nums -> nums |> List.flatten() |> Enum.map(&String.to_integer/1) end)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.map(&Integer.digits/1)
   end
 
   def max_seq_number_of(seq, size) do
@@ -32,7 +32,7 @@ defmodule Aoc25.Day03 do
       1..size
       |> Enum.map(&(List.delete_at(acc, &1 - 1) ++ [i]))
       |> then(&[acc | &1])
-      |> Enum.max_by(&Aoc.digits_to_number/1)
+      |> Enum.max_by(&Integer.undigits/1)
     end)
   end
 end
