9dd9f7ec35bbfc0e296af32023573d93482ba79d
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2025-12-08 23:03:29 -0300

    Clean Day08

--- lib/aoc25/day08.ex
+++ lib/aoc25/day08.ex
@@ -13,10 +13,9 @@ defmodule Aoc25.Day08 do
     52668
   """
   def part1(file_path, take_n \\ 1000) do
-    points = points(file_path)
-    points_distances = compare(points)
-
-    points_distances
+    file_path
+    |> points()
+    |> compare()
     |> Enum.sort_by(fn {_, value} -> value end)
     |> Enum.take(take_n)
     |> connect()
@@ -24,27 +23,6 @@ defmodule Aoc25.Day08 do
     |> Enum.sort(:desc)
     |> Enum.take(3)
     |> Enum.product()
-  end
-
-  defp points(file_path) do
-    file_path
-    |> Aoc.get_input()
-    |> Aoc.extract_numbers()
-    |> Enum.chunk_every(3)
-    |> Enum.map(&List.to_tuple/1)
-  end
-
-  defp connect(coll, acc \\ [])
-  defp connect([], acc), do: acc
-
-  defp connect([{{p1, p2}, _distance} | tail], acc) do
-    {connected?, acc} = connect_to_existing(acc, p1, p2)
-
-    if connected? do
-      connect(tail, acc)
-    else
-      connect(tail, [MapSet.new([p1, p2]) | acc])
-    end
   end
 
   @doc ~S"""
@@ -57,11 +35,24 @@ defmodule Aoc25.Day08 do
   def part2(file_path) do
     points = points(file_path)
 
+    all_connected? = fn
+      [map_set] -> length(points) == MapSet.size(map_set)
+      _ -> false
+    end
+
     points
     |> compare()
     |> Enum.sort_by(fn {_, value} -> value end)
-    |> connect_until_one(length(points), 0)
+    |> connect_until(all_connected?)
     |> then(fn {{x1, _, _}, {x2, _, _}} -> x1 * x2 end)
+  end
+
+  defp points(file_path) do
+    file_path
+    |> Aoc.get_input()
+    |> Aoc.extract_numbers()
+    |> Enum.chunk_every(3)
+    |> Enum.map(&List.to_tuple/1)
   end
 
   defp compare(coll, acc \\ %{}) do
@@ -69,45 +60,47 @@ defmodule Aoc25.Day08 do
       [_] ->
         acc
 
-      [p1 | tail] ->
-        tail
+      [p1 | rest_points] ->
+        rest_points
         |> Map.new(fn p2 -> {{p2, p1}, Aoc.euclidean_distance(p1, p2)} end)
         |> Enum.into(acc)
-        ~> compare(tail)
+        ~> compare(rest_points)
     end
   end
 
-  defp connect_to_existing(result \\ [], connected \\ MapSet.new(), existing, p1, p2) do
-    case existing do
+  defp connect(list, acc \\ []) do
+    case list do
+      [] -> acc
+      [{{p1, p2}, _distance} | rest] -> connect(rest, connect_point(acc, p1, p2))
+    end
+  end
+
+  defp connect_until([{{p1, p2}, _distance} | tail], predicate?, acc \\ []) do
+    acc = connect_point(acc, p1, p2)
+
+    if predicate?.(acc) do
+      {p1, p2}
+    else
+      connect_until(tail, predicate?, acc)
+    end
+  end
+
+  defp connect_point(result \\ [], connected \\ MapSet.new(), connections, p1, p2) do
+    case connections do
       [] ->
         if Enum.empty?(connected) do
-          {false, result}
+          [MapSet.new([p1, p2]) | result]
         else
-          {true, [connected | result]}
+          [connected | result]
         end
 
-      [head | tail] ->
-        if MapSet.member?(head, p1) or MapSet.member?(head, p2) do
-          head
-          |> MapSet.put(p1)
-          |> MapSet.put(p2)
-          |> MapSet.union(connected)
-          ~> connect_to_existing(result, tail, p1, p2)
+      [first | rest] ->
+        if MapSet.member?(first, p1) or MapSet.member?(first, p2) do
+          connected = first |> MapSet.put(p1) |> MapSet.put(p2) |> MapSet.union(connected)
+          connect_point(result, connected, rest, p1, p2)
         else
-          connect_to_existing([head | result], connected, tail, p1, p2)
+          connect_point([first | result], connected, rest, p1, p2)
         end
-    end
-  end
-
-  defp connect_until_one(coll, size, cnt, acc \\ [])
-
-  defp connect_until_one([{{p1, p2}, _distance} | tail], size, cnt, acc) do
-    {connected?, acc} = connect_to_existing(acc, p1, p2)
-
-    cond do
-      length(acc) == 1 and acc |> List.first() |> MapSet.size() == size -> {p1, p2}
-      connected? -> connect_until_one(tail, size, cnt + 1, acc)
-      :else -> connect_until_one(tail, size, cnt + 1, [MapSet.new([p1, p2]) | acc])
     end
   end
 end
