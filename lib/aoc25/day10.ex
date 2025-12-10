a72a6dbb733ae46348cbf5ae8a35aec08888f5a5
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2025-12-10 16:57:27 -0300

    Too slow to finish

--- lib/aoc25/day10.ex
+++ lib/aoc25/day10.ex
@@ -11,23 +11,68 @@ defmodule Aoc25.Day10 do
     517
   """
   def part1(file_path) do
-    items =
-      file_path
-      |> Aoc.get_input()
-      |> parse()
-
-    items
-    |> Enum.map(&search_smallest(&1, %{&1.start_switches => 0}))
+    file_path
+    |> Aoc.get_input()
+    |> parse()
+    |> Enum.map(&search_smallest_switches(&1, %{&1.start_switches => 0}))
     |> Enum.sum()
   end
 
-  defp search_smallest(item, next) do
+  defp search_smallest_switches(item, next) do
     cnt_or_acc =
       Enum.reduce_while(next, %{}, fn {state, cnt}, acc ->
         item.buttons
         |> Enum.map(&button_press(state, &1))
         |> Enum.reduce_while(acc, fn state, acc ->
-          if state == item.goal_switches do
+          cond do
+            state == item.goal_switches -> {:halt, {:halt, cnt + 1}}
+            Map.has_key?(acc, state) -> {:cont, acc}
+            :else -> {:cont, Map.put(acc, state, cnt + 1)}
+          end
+        end)
+        |> case do
+          {:halt, cnt} -> {:halt, cnt}
+          acc -> {:cont, acc}
+        end
+      end)
+
+    if is_integer(cnt_or_acc) do
+      cnt_or_acc
+    else
+      search_smallest_switches(item, cnt_or_acc)
+    end
+  end
+
+  defp button_press(state, button) do
+    Enum.reduce(button, state, fn press, state ->
+      Map.update(state, press, false, &(not &1))
+    end)
+  end
+
+  @doc ~S"""
+  ## Examples
+    iex> Aoc25.Day10.part2("example.txt")
+    33
+    # iex> Aoc25.Day10.part2("input.txt")
+    # :todo
+  """
+  def part2(file_path) do
+    file_path
+    |> Aoc.get_input()
+    |> parse()
+    |> Enum.map(&search_smallest_joltage(&1, %{&1.start_joltage => 0}))
+    |> Enum.sum()
+  end
+
+  defp search_smallest_joltage(item, next, depth \\ 0) do
+    IO.inspect(depth: depth)
+
+    cnt_or_acc =
+      Enum.reduce_while(next, %{}, fn {state, cnt}, acc ->
+        item.buttons
+        |> Enum.map(&button_press_joltage(state, &1))
+        |> Enum.reduce_while(acc, fn state, acc ->
+          if state == item.goal_joltage do
             {:halt, {:halt, cnt + 1}}
           else
             {:cont, Map.put(acc, state, cnt + 1)}
@@ -42,25 +87,14 @@ defmodule Aoc25.Day10 do
     if is_integer(cnt_or_acc) do
       cnt_or_acc
     else
-      search_smallest(item, cnt_or_acc)
+      search_smallest_joltage(item, cnt_or_acc, depth + 1)
     end
   end
 
-  defp button_press(state, button) do
+  defp button_press_joltage(state, button) do
     Enum.reduce(button, state, fn press, state ->
-      Map.update(state, press, false, &(not &1))
+      Map.update(state, press, 0, &(&1 + 1))
     end)
-  end
-
-  @doc ~S"""
-  ## Examples
-    iex> Aoc25.Day10.part2("example.txt")
-    :todo
-    iex> Aoc25.Day10.part2("input.txt")
-    :todo
-  """
-  def part2(file_path) do
-    Aoc.get_input(file_path)
   end
 
   defp parse(input) do
@@ -68,12 +102,14 @@ defmodule Aoc25.Day10 do
     |> String.split("\n")
     |> Enum.map(fn line ->
       switches = to_switches(line)
+      joltage = to_joltage(line)
 
       %{
         start_switches: Map.new(switches, fn {idx, _} -> {idx, false} end),
         goal_switches: switches,
         buttons: to_buttons(line),
-        joltage: to_joltage(line)
+        start_joltage: Map.new(joltage, fn {idx, _} -> {idx, 0} end),
+        goal_joltage: joltage
       }
     end)
   end
--- test/aoc25/day10_test.exs
+++ test/aoc25/day10_test.exs
@@ -1,5 +1,6 @@
 defmodule Aoc25.Day10Test do
   use ExUnit.Case, async: true
 
+  @moduletag timeout: :infinity
   doctest Aoc25.Day10
 end
