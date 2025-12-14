ccdb22a73eb63d4b68e49304da852cdf3791e0bf
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2025-12-14 07:57:42 -0300

    Day 12

--- lib/aoc25/day12.ex
+++ lib/aoc25/day12.ex
@@ -10,17 +10,12 @@ defmodule Aoc25.Day12 do
   def part1(file_path) do
     file_path
     |> Aoc.get_input()
+    |> String.split("\n\n")
+    |> List.last()
     |> String.split("\n")
-    |> Enum.filter(&String.contains?(&1, "x"))
     |> Enum.map(&Aoc.extract_numbers/1)
-    |> Aoc.keep(fn [n1, n2 | nums] ->
-      area = n1 * n2
-      presents = Enum.sum(nums)
-
-      if area >= presents * 9 do
-        1
-      end
-    end)
+    |> Enum.map(fn [n1, n2 | nums]-> {n1 * n2, Enum.sum(nums)} end)
+    |> Enum.map(fn {area, presents} -> %{true: 1, false: 0}[area >= presents * 9] end)
     |> Enum.sum()
   end
 end
